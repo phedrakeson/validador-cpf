426e2abf7068046ad2460a668244bd97189479f7
phedrakeson <gabrield.developer@gmail.com>
2020-12-24 11:00:39 -0300

    limpando, construindo, formatando e validando cpf

---
+++ scripts/modules/validar-cpf.js
@@ -0,0 +1,44 @@
+export default class ValidarCPF {
+  constructor(element) {
+    this.element = element
+  }
+
+  limpar(cpf) {
+    return cpf.replace(/\D/g, '');
+  }
+
+  construir(cpf) {
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+  }
+
+  formatar(cpf) {
+    const cpfLimpo = this.limpar(cpf);
+    return this.construir(cpfLimpo);
+  }
+
+  validar(cpf) {
+    const matchCpf = cpf.match(/(?:\d{3}[-.\s]?){3}\d{2}/g)
+    return (matchCpf && matchCpf[0] === cpf);
+  }
+
+  validarNaMudança(cpfElement) {
+    if(this.validar(cpfElement.value)) {
+      cpfElement.value = this.formatar(cpfElement.value);
+    } else {
+
+    }
+
+  }
+
+  adicionarEvento() {
+    this.element.addEventListener('change', () => {
+      this.validarNaMudança(this.element);
+    })
+  }
+
+  iniciar() {
+    this.adicionarEvento();
+    return this;
+  }
+}
+
